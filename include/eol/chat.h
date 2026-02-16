3883d1bd102de8d111f2a2f8088c36635c1c5496
Bene <bene@datakonstruktion.com>
2026-02-16 21:35:24 +0200

    feat(chat): add chat rendering

--- include/eol/chat.h
+++ include/eol/chat.h
@@ -4,6 +4,9 @@
 #include <string>
 #include <vector>
 
+class abc8;
+class pic8;
+
 struct chat_line {
     std::string text;
     double timestamp;
@@ -15,11 +18,18 @@ class chat {
 
     void add_line(std::string_view text);
     void clear();
+    void render(pic8* surface, abc8* font);
 
   private:
     static constexpr int MAX_LINES = 200;
+    static constexpr int VISIBLE_LINES = 5;
+    static constexpr int LINE_HEIGHT = 12;
+    static constexpr int MARGIN_X = 20;
+    static constexpr int MARGIN_Y = 2;
 
     std::vector<chat_line> lines_;
 };
+
+extern chat* GameChat;
 
 #endif
