4d39a0c31663b8e6b671178a41c887eaa41d6605
Bene <bene@datakonstruktion.com>
2026-03-02 21:48:47 +0200

    feat(test): add a death test

--- src/test/test_main.cpp
+++ src/test/test_main.cpp
@@ -1,9 +1,11 @@
+#include "test/replay_data_death.h"
 #include "test/replay_data_vsync.h"
 #include "test/test_replay.h"
 #include <array>
 
 static const replay_test_case replay_tests[] = {
     {"vsync", "vsync.lev", REPLAY_DATA(replay_vsync), true, false, 847, 8015},
+    {"left volt death", "vsync.lev", REPLAY_DATA(death), false, true, 0, 78},
 };
 
 int main() {
