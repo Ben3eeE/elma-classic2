1aa6035ce81d30818595cbcc2e04947f4fd1479d
Bene <bene@datakonstruktion.com>
2026-03-02 22:02:46 +0200

    feat(test): add vsync replay test

--- src/test/test_main.cpp
+++ src/test/test_main.cpp
@@ -1,7 +1,10 @@
+#include "src/test/replay_data_vsync.h"
 #include "src/test/test_replay.h"
 #include <array>
 
-static const replay_test_case replay_tests[] = {};
+static const replay_test_case replay_tests[] = {
+    {"vsync", "vsync.lev", REPLAY_DATA(replay_vsync), true, false, 847, 8015},
+};
 
 int main() {
     int failed = run_replay_tests(replay_tests, std::size(replay_tests));
