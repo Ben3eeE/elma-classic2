1cfdb8b1499d517d4f5ecc720935f526c7148433
Bene <bene@datakonstruktion.com>
2026-03-18 20:25:41 +0200

    refactor(platform): always apply palette to SDL surface
    Apply SDL_SetPaletteColors unconditionally in palette::set() so the paletted
    surface always has correct colors, even in OpenGL mode. This is needed for
    SDL_SaveBMP to produce correct screenshots with both renderers.

--- src/platform/sdl/sdl.cpp
+++ src/platform/sdl/sdl.cpp
@@ -357,10 +357,9 @@ palette::~palette() { delete[] (SDL_Color*)data; }
 
 void palette::set() {
     CurrentPalette = this;
+    SDL_SetPaletteColors(SDLSurfacePaletted->format->palette, (const SDL_Color*)data, 0, 256);
     if (EolSettings->renderer() == RendererType::OpenGL) {
         gl_update_palette(data);
-    } else {
-        SDL_SetPaletteColors(SDLSurfacePaletted->format->palette, (const SDL_Color*)data, 0, 256);
     }
 }
 
