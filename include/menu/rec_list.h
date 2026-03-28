19f70f3eb3e07417bfd99d032496db92f4a7bed9
Bene <bene@datakonstruktion.com>
2026-03-28 11:30:45 +0200

    feat(rec_list): add replays_for_level() with naive scanning

--- include/menu/rec_list.h
+++ include/menu/rec_list.h
@@ -8,6 +8,8 @@ class rec_list {
   public:
     // Scan rec/ folder and return all .rec filenames
     static std::vector<std::string> get_replays();
+    // Return filenames for replays matching a given level_id
+    static std::vector<std::string> replays_for_level(int level_id);
 };
 
 #endif
