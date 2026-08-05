elmadev/elma-classic

feat(game): track FPS/UPS

src/game/game.cpp
@@ -5,6 +5,7 @@
 #include "eol/settings.h"
 #include "eol/status_messages.h"
 #include "game/driver.h"
+#include "game/fps.h"
 #include "level/level.h"
 #include "level/object.h"
 #include "level/segments.h"
@@ -494,6 +495,7 @@ int game_loop(const char* filename, CameraMode camera_mode) {
     }
 
     bool both_bikes_alive = true;
+    fps::reset();
     while (true) {
         // Get timestep
         double target_time = stopwatch() * 0.0024;
@@ -504,6 +506,8 @@ int game_loop(const char* filename, CameraMode camera_mode) {
         bool console_was_active = handle_console_input();
 
         while (time <= target_time - 0.000001) {
+            fps::physics_frame();
+
             // Cap slowest frame to 0.0055 (approximately 12.6 milliseconds or 79.4 fps)
             double dt = 0.0055;
             if (time + dt > target_time) {
@@ -762,6 +766,7 @@ int replay_loop(const char* filename, bool restore_player_visibility) {
     double current_replay_time = 0.0;
     double last_stopwatch = stopwatch();
 
+    fps::reset();
     while (true) {
         handle_events();
 
@@ -931,6 +936,7 @@ void render_replay(const char* level_filename) {
     driver driv1(Motor1, Rec1, &State->keys1, &HudReplay1);
     driver driv2(Motor2, Rec2, &State->keys2, &HudReplay2);
 
+    fps::reset();
     while (true) {
         handle_events();
         if (is_game_key_down(DIK_ESCAPE)) {

src/renderer/render.cpp
@@ -5,6 +5,7 @@
 #include "eol/settings.h"
 #include "eol/status_messages.h"
 #include "game/driver.h"
+#include "game/fps.h"
 #include "game/game.h"
 #include "level/level.h"
 #include "level/object.h"
@@ -836,6 +837,8 @@ static void render_view(bool player1, pic8* pic, double time, driver& driv, driv
 }
 
 void render_game(double time, driver& driv1, driver& driv2, camera& current_camera) {
+    fps::render_frame();
+
     // Determine who we are going to draw (player 1, player 2 or both)
     bool draw_player1 = driv1.draw_view;
     bool draw_player2 = driv2.draw_view;

Ben3eeE/elma-classic2

src/game/game.cpp
@@ -5,6 +5,7 @@
 #include "eol/settings.h"
 #include "eol/status_messages.h"
 #include "game/driver.h"
+#include "game/fps.h"
 #include "level/level.h"
 #include "level/object.h"
 #include "level/segments.h"
@@ -508,6 +509,7 @@ int game_loop(const char* filename, CameraMode camera_mode) {
     }
 
     bool both_bikes_alive = true;
+    fps::reset();
     while (true) {
         const bool frozen = time == 0.0 && current_camera.mode != CameraMode::MapViewer &&
                             EolClient->bike_frozen_by_countdown();
@@ -537,6 +539,7 @@ int game_loop(const char* filename, CameraMode camera_mode) {
                     continue;
                 }
 
+                fps::count_ups();
                 if (!driv1.dead) {
                     physics_subframe(driv1, time, dt);
                 }
@@ -797,6 +800,7 @@ int replay_loop(const char* filename, bool restore_player_visibility) {
     double current_replay_time = 0.0;
     double last_stopwatch = stopwatch();
 
+    fps::reset();
     while (true) {
         handle_events();
 
@@ -966,6 +970,7 @@ void render_replay(const char* level_filename) {
     driver driv1(Motor1, Rec1, &State->keys1, &HudReplay1);
     driver driv2(Motor2, Rec2, &State->keys2, &HudReplay2);
 
+    fps::reset();
     while (true) {
         handle_events();
         if (is_game_key_down(DIK_ESCAPE)) {

src/renderer/render.cpp
@@ -5,6 +5,7 @@
 #include "eol/settings.h"
 #include "eol/status_messages.h"
 #include "game/driver.h"
+#include "game/fps.h"
 #include "game/game.h"
 #include "level/level.h"
 #include "level/object.h"
@@ -838,6 +839,8 @@ static void render_view(bool player1, pic8* pic, double time, driver& driv, driv
 }
 
 void render_game(double time, driver& driv1, driver& driv2, camera& current_camera) {
+    fps::count_fps();
+
     // Determine who we are going to draw (player 1, player 2 or both)
     bool draw_player1 = driv1.draw_view;
     bool draw_player2 = driv2.draw_view;